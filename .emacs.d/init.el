120d7ebc46612c217ea5d22b62f6e20e0ccfa905
Scott Bale <scott@balehaus.org>
2014-07-04 11:57:13 -0500

    update emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings color-theme-solarized nrepl haskell-mode markdown-mode js2-mode)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings zenburn-theme clojure-mode cider paredit haskell-mode markdown-mode js2-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -21,6 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" default)))
+ '(inferior-lisp-program "java -cp TODO init.el clojure.main")
  '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
