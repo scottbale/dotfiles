35ea084a5aef2b31936301b2995ea1cc476c0326
Scott Bale <scott@balehaus.org>
2014-07-16 10:52:14 -0500

    emacs php-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings zenburn-theme clojure-mode cider paredit haskell-mode markdown-mode js2-mode)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings zenburn-theme clojure-mode cider paredit haskell-mode markdown-mode js2-mode php-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
--- .emacs.d/scott.el
+++ .emacs.d/scott.el
@@ -26,6 +26,7 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
 
 ;; no auto-fill-mode ever
 (auto-fill-mode -1)
