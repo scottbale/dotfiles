932f6aa3d521b4bbac1d3282c06fd27517d197d6
Scott Bale <scott@balehaus.org>
2015-06-01 07:19:21 -0700

    include cider in emacs my-packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings zenburn-theme clojure-mode paredit haskell-mode markdown-mode js2-mode php-mode)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings zenburn-theme cider clojure-mode paredit haskell-mode markdown-mode js2-mode php-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
