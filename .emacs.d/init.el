b0684ff6260556a6d9232bfd5bda5dc30b069e70
Scott Bale <scott@balehaus.org>
2015-06-01 07:13:21 -0700

    do away with .emacs.d/<username>.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,3 +34,42 @@
 (if window-system
     (load-theme 'zenburn t)
   (load-theme 'zenburn t))
+
+
+;; below this line are the previous contents of <username>.el
+;; ----------------------------------------------------------
+
+;; (global-set-key (kbd "<C-tab>") 'other-window)
+(global-set-key (kbd "<C-M-return>") 'new-frame)
+;; ------------------------------------------
+(global-set-key (kbd "C-S-f") 'rgrep)
+;; see also:
+;; help v [describe variable] case-fold-search
+;; M-x set-variable
+;; ------------------------------------------
+;; (global-set-key (kbd "C-S-n") 'locate)
+(global-set-key (kbd "C-S-n") 'find-name-dired)
+
+;; File finding - from technomancy starter kit 1.x, removed in 2.x
+(recentf-mode t)
+(global-set-key (kbd "C-x f") 'recentf-open-files)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(set-face-attribute 'default nil :height 120)
+
+(autoload 'markdown-mode "gfm-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+
+;; no auto-fill-mode ever
+(auto-fill-mode -1)
+(remove-hook 'text-mode-hook 'turn-on-auto-fill)
+
+(add-hook 'html-mode-hook
+          (lambda ()
+            ;; Default indentation is usually 2 spaces, changing to 4.
+            (set (make-local-variable 'sgml-basic-offset) 4)))
