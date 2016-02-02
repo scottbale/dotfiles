e0f92d6c7fedca1018e443b98a221d66a1cf64e3
Scott Bale <scott@balehaus.org>
2016-02-02 09:07:23 -0600

    emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,80 @@
+;; https://github.com/technomancy/emacs-starter-kit
+
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '( starter-kit starter-kit-lisp starter-kit-bindings zenburn-theme cider clojure-mode paredit haskell-mode markdown-mode js2-mode php-mode  ) 
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "085b401decc10018d8ed2572f65c5ba96864486062c0a2391372223294f89460" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" default)))
+ '(inferior-lisp-program "java -cp TODO init.el clojure.main")
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "http://melpa-stable.milkbox.net/packages/"))))
+ '(tab-width 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(if window-system
+    (load-theme 'zenburn t)
+  (load-theme 'zenburn t))
+
+
+;; below this line are the previous contents of <username>.el
+;; ----------------------------------------------------------
+
+;; (global-set-key (kbd "<C-tab>") 'other-window)
+(global-set-key (kbd "<C-M-return>") 'new-frame)
+;; ------------------------------------------
+(global-set-key (kbd "C-S-f") 'rgrep)
+;; see also:
+;; help v [describe variable] case-fold-search
+;; M-x set-variable
+;; ------------------------------------------
+;; (global-set-key (kbd "C-S-n") 'locate)
+(global-set-key (kbd "C-S-n") 'find-name-dired)
+
+;; File finding - from technomancy starter kit 1.x, removed in 2.x
+(recentf-mode t)
+(global-set-key (kbd "C-x f") 'recentf-open-files)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(set-face-attribute 'default nil :height 120)
+
+(autoload 'markdown-mode "gfm-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+
+;; no auto-fill-mode ever
+(auto-fill-mode -1)
+(remove-hook 'text-mode-hook 'turn-on-auto-fill)
+
+(add-hook 'html-mode-hook
+          (lambda ()
+            ;; Default indentation is usually 2 spaces, changing to 4.
+            (set (make-local-variable 'sgml-basic-offset) 4)))
