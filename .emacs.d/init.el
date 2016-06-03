c7848f4af76e5789d4c456055d75a1592dbd1234
Scott Bale <scott.bale@datastax.com>
2016-06-02 22:26:34 -0500

    paredit stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,6 +79,15 @@
           (lambda ()
             (idle-highlight-mode t)))
 
+;; paredit stuff
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(add-hook 'scheme-mode-hook 'enable-paredit-mode)
+(add-hook 'emacs-lisp--mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(eval-after-load 'paredit
+  ;; need a binding that works in the terminal
+  '(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp))
+
 ;; ffip
 (setq ffip-prefer-ido-mode t)
 (global-set-key (kbd "C-S-n") 'find-file-in-project)
