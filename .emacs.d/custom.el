aed123b5c9595ca8f4b12fa692abb698feecfb0f
Scott Bale <scott.bale@datastax.com>
2017-02-25 11:50:44 -0600

    ditch ido in favor of ivy

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,25 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "085b401decc10018d8ed2572f65c5ba96864486062c0a2391372223294f89460" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" default)))
- '(inferior-lisp-program "java -cp TODO init.el clojure.main")
- '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa-stable.milkbox.net/packages/"))))
  '(package-selected-packages
    (quote
-    (ruby-test-mode zenburn-theme yaml-mode swiper smex php-mode paredit markdown-mode js2-mode ido-ubiquitous idle-highlight-mode haskell-mode find-file-in-project color-theme-solarized cider better-defaults)))
+    (find-file-in-project counsel swiper ivy helm-config zenburn-theme yaml-mode ruby-test-mode php-mode paredit markdown-mode js2-mode idle-highlight-mode haskell-mode color-theme-solarized cider better-defaults)))
  '(safe-local-variable-values
    (quote
-    ((ffip-patterns "*.html" "*.js" "*.json" "*.css" "*.scss" "*.java" "*.xml" "*.js" "*.py" "*.clj" "*.sh" "*.txt" "*.md" "*.yaml" "*.cfg" "*.conf" "*.env" "*.log" "*.properties" "*.dita" "*.ditamap")
-     (ffip-patterns "*.html" "*.js" "*.json" "*.css" "*.scss" "*.java" "*.xml" "*.js" "*.py" "*.clj" "*.sh" "*.txt" "*.md" "*.yaml" "*.cfg" "*.conf" "*.env" "*.log" "*.properties")
-     (ffip-patterns "*.html" "*.js" "*.json" "*.css" "*.scss" "*.java" "*.xml" "*.js" "*.py" "*.clj" "*.sh" "*.txt" "*.md" "*.yaml" "*.cfg" "*.conf" "*.env" "*.log")
-     (ffip-prune-patterns "*/.git/*" "*/node_modules/*" "*/node_modules-compiled/*" "*/ripcord-compiled/*" "*/spock-compiled/*" "*/.sass-cache/*" "*/target/*" "*/3rd_party/*" "*/bower_components/*" "*/Lib/py/*" "*/python-libraries/*")
-     (ffip-patterns "*.html" "*.js" "*.json" "*.css" "*.scss" "*.java" "*.xml" "*.js" "*.py" "*.clj" "*.sh" "*.txt" "*.md" "*.yaml" "*.log"))))
- '(tab-width 4))
+    ((ffip-prune-patterns "*/.git/*" "*/node_modules/*" "*/node_modules-compiled/*" "*/ripcord-compiled/*" "*/spock-compiled/*" "*/.sass-cache/*" "*/target/*" "*/3rd_party/*" "*/bower_components/*" "*/Lib/py/*" "*/python-libraries/*")
+     (ffip-patterns "*.html" "*.js" "*.json" "*.css" "*.scss" "*.java" "*.xml" "*.js" "*.py" "*.clj" "*.sh" "*.txt" "*.md" "*.yaml" "*.cfg" "*.conf" "*.env" "*.log" "*.properties" "*.dita" "*.ditamap")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '( better-defaults smex ido-ubiquitous idle-highlight-mode find-file-in-project
+(defvar my-packages '( better-defaults idle-highlight-mode ivy swiper counsel find-file-in-project
                                        zenburn-theme color-theme-solarized cider clojure-mode paredit
                                        haskell-mode markdown-mode js2-mode php-mode yaml-mode) 
   "A list of packages to ensure are installed at launch.")
@@ -24,6 +24,7 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; use solarized dark in windows, zenburn in terminal (solarized doesn't look right in terminal)
 (if window-system
     (load-theme 'solarized-dark t)
   (load-theme 'zenburn t))
@@ -33,13 +34,22 @@
 
 ;; keybindings I got used to from technomancy starter kit
 (recentf-mode t)
-(global-set-key (kbd "C-x f") 'recentf-open-files)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-(global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-c r") 'revert-buffer)
+
+;; ivy
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-x f") 'ivy-recentf)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
 
 (set-face-attribute 'default nil :height 120)
 
+;; markdown mode
 (autoload 'gfm-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
@@ -71,7 +81,6 @@
   '(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp))
 
 ;; ffip
-(setq ffip-prefer-ido-mode t)
 (global-set-key (kbd "C-S-n") 'find-file-in-project)
 
 ;; for flyspell in gfm-mode
