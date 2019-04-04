512667476890ccb6cac950dd76df9496915d0bd9
Scott Bale <scott.bale@datastax.com>
2019-04-04 17:01:13 -0500

    necessary to explicitly set prefix for projectile 2.0

--- .emacs.d/settings/setup-projectile.el
+++ .emacs.d/settings/setup-projectile.el
@@ -2,4 +2,6 @@
 (setq projectile-completion-system 'ivy)
 ;; (global-set-key (kbd "C-S-n") 'projectile-discover-projects-in-directory)
 
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
 (provide 'setup-projectile)
